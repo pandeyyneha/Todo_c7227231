941a5e02ba7132bccc8480340b1edafd3c5da398
Admin <neha71p@gmail.com>
2022-05-16 00:41:30 +0545

    todo task

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.todo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.todo.database.Todo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, TodoFragment.newInstance())
+                    .commitNow();
+        }
+    }
+    public void moveToUpdate(Todo todo) {
+        Bundle bundle=new Bundle();
+        bundle.putSerializable("todo",todo);
+        Fragment fragment= new UpdateFragment().newInstance();
+        fragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+}
